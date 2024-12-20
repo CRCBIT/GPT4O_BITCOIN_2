c7510b7725ae08ab2dfefde8aeed0a855d9b2d6b
CRCBIT <eogml65114@gmail.com>
2024-12-20 16:24:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -270,7 +270,7 @@ def main():
                 range=y_range  # 동적으로 계산된 y축 범위 적용
             ),
             margin=dict(l=20, r=20, t=0, b=50),
-            height=400,  # 차트 높이 축소
+            height=350,  # 차트 높이 축소
             hovermode="x unified",
             showlegend=False,
             plot_bgcolor='rgba(0,0,0,0)',  # 투명 배경
